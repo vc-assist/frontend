84d396da3297ccf95c57c469bf2e3cd3fd169566
ThatXliner <bryan.hu.2020@gmail.com>
2024-11-28 14:44:21 -0800

    :zap: Base on user session token?

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -1,20 +1,24 @@
 import { useQuery } from "@tanstack/react-query"
 import { useAtomValue } from "jotai"
-import { DataModulesAtom } from "./stores"
+import { DataModulesAtom, UserAtom } from "./stores"
 
 export function usePowerSchoolQuery() {
+  const { token } = useAtomValue(UserAtom)
   const dataModules = useAtomValue(DataModulesAtom)
+  // Should never happen
   if (!dataModules?.powerschool) return null
   return useQuery({
-    queryKey: ["powerschool"],
+    queryKey: ["powerschool", token],
     queryFn: dataModules.powerschool.get,
   })
 }
 export function useMoodleQuery() {
+  const { token } = useAtomValue(UserAtom)
   const dataModules = useAtomValue(DataModulesAtom)
+  // Should never happen
   if (!dataModules?.moodle) return null
   return useQuery({
-    queryKey: ["moodle", dataModules.moodle],
+    queryKey: ["moodle", token],
     queryFn: dataModules.moodle.get,
   })
 }
